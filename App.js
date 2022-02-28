62c707b262f738f58c2f58394520b9be9419ccad
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-28 20:51:39 +0000

    add detailed comments to help improve understanding of useState and useEffect

--- App.js
+++ App.js
@@ -2,32 +2,59 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-   // would this work though because page shoud always be reading a random api url, it should'nt be empty
-  // set variable to an empty string
-  const [refresh, setRefresh] = useState(true);
-  // if variable refresh equals empty string then set it to random img using api
-  /*
-  if (refresh === "") {
-    setRefresh("https://dog.ceo/api/breeds/image/random");
-  }
-  */
+  // what is useState?
+    // It declares a “state variable”. This is a way to “preserve” some values between the function calls
+    // The state doesn't have to be an object, it can be a string or a number if that is what you need
+    // It returns a pair of values: the current state and a function that updates it
+      // In this case, 'refresh' is the current state and 'setRefresh' is the function
+    /* We declare a state variable called refresh, and set it to true.
+    React will remember its current value between re-renders, and provide the most recent one to our function.
+    If we want to update the current refresh, we can call setRefresh.
+    */
 
-  // set variable to an empty string
+  // Why is useState set to true?
+    // When called with a s value the state will be set (to what you have put it to) and the component will re-render.
+    // When refresh runs and the value is 'false' the function setRefresh is called into action
+    // with this useState it will always expect a boolean value
+  const [refresh, setRefresh] = useState(true);
+
+  // Set variable to an empty string
+    // With this useState it will always expect a string value
   const [newDog, setNewDog] = useState("");
-  // if variable url equals empty string, set setUrl to what you want it to show
+
+  // What is useEffect?
+    // The Effect Hook lets you perform side effects in function components
   useEffect(() => {
+    // What is 'const res'?
+      // It is a function that you can call
+    // what does async do?
+      //
     const res = async () => {
+      // What is 'const fetchRes'
+        // the varable that is set to fetch which is where you are pulling the data from using an API
+      // What does await & fetch do
+        // await is telling the fetch method to wait until JavaScript has read all the code and not to go ahead with a call
       const fetchRes = await fetch('https://dog.ceo/api/breeds/image/random')
 
+      // What is 'const data'
+        // the variable in which the object is being stored (read below)
+      // What is .jons()
+        // .json() tells JavaSvript to change data into JavaScript Objects so that your code can read that data 
       const data = await fetchRes.json();
-      console.log(data);
-    
-      const dog = data.message
-      console.log(dog);
+      //console.log(data);
       
+      // What is data.message doing?
+        // is is accessing the .mesasge property of data, which is the api that has been turned into a JS object so that the code can be read
+      const dog = data.message
+      //console.log(dog);
+      
+      // why call setNewDog with (dog)?
       setNewDog(dog);
     }
+    //why call res()?
     res()
+
+  // what is [refresh]?
   }, [refresh]);
 
   return (
