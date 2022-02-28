ebe7ea311730b2acd361809cc0a021847451dcd5
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-02-28 19:40:51 +0000

    update files

--- App.js
+++ App.js
@@ -1,43 +1,49 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+   // would this work though because page shoud always be reading a random api url, it should'nt be empty
+  // set variable to an empty string
+  const [refresh, setRefresh] = useState(true);
+  // if variable refresh equals empty string then set it to random img using api
+  /*
+  if (refresh === "") {
+    setRefresh("https://dog.ceo/api/breeds/image/random");
+  }
+  */
 
   // set variable to an empty string
-  const [url, setUrl] = useState('')
-    // if variable url equals empty string, set setUrl to what you want it to show
-    useEffect(() => {
-      if(url === ''){
-        console.log('here')
-        setUrl('https://dog.ceo/api/breeds/image/random');
-      }
-    });
-  
+  const [newDog, setNewDog] = useState("");
+  // if variable url equals empty string, set setUrl to what you want it to show
+  useEffect(() => {
+    const res = async () => {
+      const fetchRes = await fetch('https://dog.ceo/api/breeds/image/random')
 
-
-
-
-  // would this work thought because page shoud always be reading a random api url, it should'nt be empty
-  // set variable to an empty string
-  const [refresh, setRefresh] = useState('')
-    // if variable refresh equals empty string then set it to random img using api
-    if(refresh === ''){
-      console.log('refresh')
-      setRefresh('https://dog.ceo/api/breeds/image/random')
+      const data = await fetchRes.json();
+      console.log(data);
+    
+      const dog = data.message
+      console.log(dog);
+      
+      setNewDog(dog);
     }
+    res()
+  }, [refresh]);
 
-    return (
-      <div className="App">
-          <title>Random Dog Generator</title>
-          <div className="container">
-            <h1>Random Dog Generator</h1>
-            <div className="buttonContainer">
-              <button id="dogButton" type="button" onClick={setRefresh}>Another Doggo Please!</button>
-            </div>
-            <ol id="dogCard"></ol>
-            <img id="doggo" alt="A happy dog!" src={url}/>
+  return (
+    <div className="App">
+      <title>Random Dog Generator</title>
+      <div className="container">
+        <h1>Random Dog Generator</h1>
+        <div className="buttonContainer">
+          <button id="dogButton" type="button" onClick={() => setRefresh(!refresh)}> 
+            Another Doggo Please!
+          </button>
         </div>
+        <ol id="dogCard"></ol>
+        <img id="doggo" alt="A happy dog!" src={newDog} />
       </div>
+    </div>
   );
 }
 
